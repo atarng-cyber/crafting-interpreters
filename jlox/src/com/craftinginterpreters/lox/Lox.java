a2de48924a4593dda06c8e8688d07b9dad0f89d7
Andrew <andrew@Andrews-MacBook-Pro.local>
2026-02-09 17:31:22 -0800

    Chapter 6 code updates

--- jlox/src/com/craftinginterpreters/lox/Lox.java
+++ jlox/src/com/craftinginterpreters/lox/Lox.java
@@ -47,17 +47,30 @@ public class Lox {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
 
-    // For now, just print the tokens.
-    for (Token token : tokens) {
-      System.out.println(token);
-    }
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
+
+    // Stop if there was a syntax error.
+    if (hadError) return;
+
+    System.out.println(new AstPrinter().print(expression));
   }
 
   static void error(int line, String message) {
     report(line, "", message);
   }
 
-  private static void report(int line, String where, String message) {
+  // Report an error at a specific token (used by the parser).
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
+  }
+
+  private static void report(int line, String where,
+                             String message) {
     System.err.println("[line " + line + "] Error" + where + ": " + message);
     hadError = true;
   }
